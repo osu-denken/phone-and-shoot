481ad43a0406c7f2a2d18a2a6c0b1cf0eeb9bb2f
kmmz532 <mizuto1127no01@gmail.com>
2026-06-05 17:25:33 +0900

    first commit

---
+++ asset/scripts/sprites.js
@@ -0,0 +1,30 @@
+window.spritePaths = {
+  1: '/asset/images/character-1.png',
+  2: '/asset/images/character-2.png',
+  3: '/asset/images/character-3.png',
+  4: '/asset/images/character-4.png',
+  5: '/asset/images/character-5.png',
+  6: '/asset/images/character-6.png',
+  7: '/asset/images/character-7.png',
+  8: '/asset/images/character-8.png',
+};
+
+window.itemSpritePaths = {
+  health_potion: '/asset/images/health_potion.png',
+  health_increase: '/asset/images/heart_increase.png',
+  score_up: '/asset/images/score_up.png',
+  shield: '/asset/images/shield.png',
+  triple_shot: '/asset/images/triple_shot.png',
+  empty: '/asset/images/empty.png',
+};
+
+window.guideSpritePaths = {
+  phone: '/asset/images/phone.png',
+  arrow: '/asset/images/arrow.png',
+};
+window.bulletSpritePath = '/asset/images/bullet.png';
+window.enemyBulletSpritePath = '/asset/images/enemy_bullet.png';
+window.stageBackgroundPaths = {
+  2: '/asset/images/stage-second.png',
+  3: '/asset/images/stage-final.png',
+};
